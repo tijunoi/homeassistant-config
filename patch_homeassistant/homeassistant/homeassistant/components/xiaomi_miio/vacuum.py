c89955fcd8867d389102df7ac2dc10cd1471d48c
Nil Ordoñez <nilordonez.7@gmail.com>
2022-09-18 17:42:40 +0200

    Update homeassistant to 2022.8.7

--- patch_homeassistant/homeassistant/homeassistant/components/xiaomi_miio/vacuum.py
+++ patch_homeassistant/homeassistant/homeassistant/components/xiaomi_miio/vacuum.py
@@ -86,11 +86,9 @@ async def async_setup_entry(
     entities = []
 
     if config_entry.data[CONF_FLOW_TYPE] == CONF_DEVICE:
-        name = config_entry.title
         unique_id = config_entry.unique_id
 
         mirobo = MiroboVacuum(
-            name,
             hass.data[DOMAIN][config_entry.entry_id][KEY_DEVICE],
             config_entry,
             unique_id,
@@ -202,14 +200,13 @@ class MiroboVacuum(
 
     def __init__(
         self,
-        name,
         device,
         entry,
         unique_id,
         coordinator: DataUpdateCoordinator[VacuumCoordinatorData],
     ):
         """Initialize the Xiaomi vacuum cleaner robot handler."""
-        super().__init__(name, device, entry, unique_id, coordinator)
+        super().__init__(device, entry, unique_id, coordinator)
         self._state: str | None = None
         self._active_rooms = None
 
